9de93e9462dd7c97ce4ef1c23d900febeb235e36
Mohammed Ahteshamuz Zaman <ahteshamzaman2024@gmail.com>
2026-02-15 17:40:00 +0530

    used agentexecutor, hub and create_react_agent to make a new search agent

--- main.py
+++ main.py
@@ -4,32 +4,45 @@ load_dotenv()
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 tools = [TavilySearch()]
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 react_prompt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
 
 agent = create_react_agent(
     llm=llm,
     tools=tools,
-    prompt=react_prompt,
+    prompt=react_prompt_with_format_instructions,
 )
 
 agent_executor = AgentExecutor(
-    agent=agent, 
-    tools=tools, 
+    agent=agent,
+    tools=tools,
     verbose=True,
     handle_parsing_errors=True,
 )
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_output
 
-chain = agent_executor
 
 def main():
     result = chain.invoke(
         input={
-            "input": "Search for 3 jobs for an ai engineer using langchain in banglore or hyderabad area and list there details with apply link"
+            "input": "Search for 3 jobs for an ai engineer using langchain in banglore or hyderabad area and list there details"
         },
     )
     print(result)
@@ -37,4 +50,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
